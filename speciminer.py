7f010c43811e11f0141d0cf90da5ef1809db0512
adamancer <mansura@si.edu>
2018-05-01 10:04:05 -0400

    Moved document fetching to postflight script

--- speciminer.py
+++ speciminer.py
@@ -5,23 +5,6 @@ import glob
 import os
 import re
 
-import requests
-
-
-def get_documents(**kwargs):
-    """Returns metadata about a set of GeoDeepDive documents"""
-    url = 'https://geodeepdive.org/api/articles'
-    response = requests.get(url, params=kwargs)
-    if  response.status_code == 200:
-        return response.json().get('success', {}).get('data', [])
-    return []
-
-
-def get_document(doc_id):
-    """Returns metadata about a single GeoDeepDive document"""
-    docs = get_documents(id=doc_id)
-    return docs[0] if docs else {}
-
 
 # Preflight
 for dirname in ['input', 'output']:
@@ -38,11 +21,6 @@ for fp in glob.iglob(os.path.join('input', '*')):
         # Find USNM specimens
         matches = re.findall(pattern, f.read())
         specimens = list(set([sorted(m, key=len)[-1].strip() for m in matches]))
-        # Get document metadata from the GeoDeepDive website
-        doc_id = os.path.splitext(os.path.basename(fp))[0]
-        doc = get_document(doc_id)
-        for specimen in specimens:
-            output.append([doc_id, doc.get('journal'), specimen])
 
 # Write results to file
 with open(os.path.join('output', 'cited.csv'), 'wb') as f:
