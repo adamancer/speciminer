1c7464812f39e9035b47e703fcca208b8efc91f5
adamancer <mansura@si.edu>
2018-05-25 11:39:30 -0400

    Improved range parsing (including shorthand)

--- specimens.py
+++ specimens.py
@@ -1,5 +1,8 @@
 """Defines methods to work with USNM catalog numbers and specimen data"""
+# FIXME: Allow user to disable short range (e.g., for Mineral Sciences)
 
+
+import math
 import os
 import re
 import time
@@ -33,9 +36,17 @@ class Parser(object):
         self.discrete = re.compile(self.regex['discrete_mask'].format(**self.regex))
         self.range = re.compile(self.regex['range_mask'].format(**self.regex))
         self.code = ''
+        self.metadata = []
+        self.expand_short_ranges = True
+
+
+    def sprint(self, *msg):
+        if self.regex['debug']:
+            print ' '.join([s if isinstance(s, basestring) else repr(s) for s in msg])
 
 
     def findall(self, val):
+        """Finds all likely catalog numbers within the given string"""
         matches = []
         for match in set([m[0] for m in self.mask.findall(val)]):
             if re.search('\d', match):
@@ -43,8 +54,10 @@ class Parser(object):
         return sorted(matches)
 
 
-    def parse(self, val):
+    def parse(self, val, expand_short_ranges=True):
         """Parses catalog numbers from a string"""
+        self.expand_short_ranges = expand_short_ranges
+        val = val.decode('utf-8')
         try:
             return self._parse(val)
         except ValueError:
@@ -53,18 +66,26 @@ class Parser(object):
 
     def _parse(self, val):
         """Parses catalog numbers from a string"""
-        self.code = re.findall(self.regex['prefix'], val)[0]
+        self.sprint('Parsing {}...'.format(val))
+        self.code = re.findall(self.regex['code'], val)[0]
+        self.metadata = []
         nums = []
         for match in [m[0] for m in self.mask.findall(val)]:
+            # The museum code interferes with parsing, so strip it here
+            match = self.remove_museum_code(match)
             nums.extend(self.parse_discrete(match))
             nums.extend(self.parse_ranges(match))
+        if not nums and self.is_range(val):
+            self.sprint('Parsed as simple range:', val)
+            nums = self.fill_range(val)
         if not nums:
-            spec_num = self.split_num(val)
-            nums = ['{}{}-{}'.format(spec_num.prefix,
-                                     spec_num.number,
-                                     spec_num.suffix).strip('-')]
-        nums = [u'{} {}'.format(self.code, n) for n in nums]
-        nums = sorted(list(set(nums)))
+            self.sprint('Parsed as catalog number:', val)
+            nums = [self.parse_num(val)]
+        # Are the lengths in the results reasonable?
+        if len(nums) > 1:
+            maxlen = max([len(str(n.number)) for n in nums])
+            nums = [n for n in nums if n.number > 10**(maxlen - 2)]
+        nums = sorted(list(set([self.stringify(n) for n in nums])))
         return nums
 
 
@@ -88,32 +109,58 @@ class Parser(object):
 
     def parse_discrete(self, val):
         """Returns a list of discrete specimen numbers"""
+        val = re.sub(self.regex['filler'], '', val)
+        prefix = re.match('(' + self.regex['prefix'] + ')', val)
+        prefix = prefix.group() if prefix is not None else''
         discrete = self.discrete.search(val)
         nums = []
         if discrete is not None:
-            for spec_num in re.findall(self.regex['number'], discrete.group()):
+            val = discrete.group().strip()
+            val = self.cluster(self.fix_ocr_errors(val))
+            if re.match(self.regex['catnum'] + '$', val):
+                # Check if value is actually a single catalog number
+                nums.append(self.parse_num(val.replace(' ', '')))
+            else:
                 # Check if specimen number is actually a range
-                if self.is_range(spec_num):
-                    nums.extend(self.fill_range(spec_num))
-                else:
-                    nums.append(spec_num.strip())
+                for spec_num in re.findall(self.regex['catnum'], val):
+                    if not spec_num.startswith(prefix):
+                        spec_num = prefix + spec_num
+                    if self.is_range(spec_num):
+                        nums.extend(self.fill_range(spec_num))
+                    elif re.match(self.regex['catnum'] + '$', spec_num.strip()):
+                        nums.append(self.parse_num(spec_num.replace(' ', '')))
+                    else:
+                        nums.append(self.parse_num(spec_num))
+        if nums:
+            self.sprint('Parsed discrete:', [self.stringify(n) for n in nums])
         return nums
 
 
     def parse_ranges(self, val):
         """Returns a list of specimen numbers given in ranges"""
+        val = self.cluster(self.fix_ocr_errors(val))
         ranges = self.range.search(val)
         nums = []
         if ranges is not None:
-            spec_num = ranges.group()
-            try:
-                n1, n2 = re.findall(self.regex['number'], spec_num)
-                n1, n2 = [self.split_num(n) for n in (n1, n2)]
-            except ValueError:
-                if self.is_range(spec_num):
-                    nums.extend(self.fill_range(spec_num))
+            spec_num = ranges.group().strip()
+            if self.is_range(spec_num):
+                nums.extend(self.fill_range(spec_num))
             else:
-                nums.extend(self.fill_range(n1, n2))
+                # Catch legitimate specimen numbers. Short ranges are caught
+                # above, so anything that parses should be excluded here.
+                try:
+                    parsed = self.parse_num(spec_num)
+                except ValueError:
+                    # Finds ranges joined by something other than a dash
+                    try:
+                        n1, n2 = re.findall(self.regex['catnum'], spec_num)
+                        n1, n2 = [self.parse_num(n) for n in (n1, n2)]
+                    except ValueError:
+                        pass
+                    else:
+                        nums.extend(self.fill_range(n1, n2))
+        if nums:
+            self.sprint('Parsed range:', [self.stringify(n) for n in nums])
         return nums
 
 
@@ -158,10 +205,10 @@ class Parser(object):
 
 
     def remove_museum_code(self, val):
+        """Strips the museum code from the beginning of a value"""
         if self.code:
-            return val.replace(self.code, '', 1).strip(' -')
-        return re.sub(self.regex['prefix'], '', val).strip(' -')
-
+            return val.replace(self.code, '', 1).replace('()', '').strip(' -')
+        return re.sub(self.regex['code'], '', val).replace('()', '').strip(' -')
 
 
     def parse_num(self, val):
@@ -224,34 +271,72 @@ class Parser(object):
 
     def fill_range(self, n1, n2=None):
         """Fills a catalog number range"""
+        derived_n2 = False
         if n2 is None:
-            n1, n2 = [n.strip() for n in n1.split('-')]
-            n1, n2 = [self.split_num(n) for n in (n1, n2)]
+            n1, n2 = self.get_range(n1)
+            derived_n2 = True
         if n1.prefix and not n2.prefix:
             n2 = SpecNum(n2.code, n1.prefix, n2.number, n2.suffix)
         if self.is_range(n1, n2):
-            return ['{}{}'.format(n1.prefix, n) for n in xrange(n1.number, n2.number + 1)]
-        return [n1, n2]
+            return [SpecNum(self.code, n1.prefix, n, '')
+                    for n in xrange(n1.number, n2.number + 1)]
+        # Range parse failed!
+        return [n1, n2] if not derived_n2 else [n1]
+
+
+    def get_range(self, n1, n2=None):
+        """Generates all the catalog numbers in a catalog number range"""
+        if n2 is None:
+            n1, n2 = self.split_num(n1)
+        if not self._is_range(n1, n2):
+            n1, n2 = self.short_range(n1, n2)
+        return n1, n2
+
+
+    def split_num(self, val, delim='-'):
+        """Splits the catalog number and suffix for range testing"""
+        n1, n2 = [n.strip() for n in val.strip().split(delim)]
+        n1, n2 = [self.parse_num(n) for n in (n1, n2)]
+        if n1.prefix and not n2.prefix:
+            n2 = SpecNum(n2.code, n1.prefix, n2.number, n2.suffix)
+        return n1, n2
 
 
     def is_range(self, n1, n2=None):
         """Tests if a given value is likely to be a range"""
         if n2 is None:
             try:
-                n1, n2 = [n.strip() for n in n1.split('-')]
+                n1, n2 = self.split_num(n1)
             except ValueError:
                 return False
-            else:
-                n1, n2 = [self.split_num(n) for n in (n1, n2)]
+        is_range = self._is_range(n1, n2)
+        if not is_range and self.expand_short_ranges:
+            is_range = self._is_range(*self.short_range(n1, n2))
+        return is_range
+
+
+    def _is_range(self, n1, n2=None):
+        """Tests if a given pair of numbes are likely to be a range"""
+        if n2 is None:
+            n1, n2 = self.split_num(n1)
         same_prefix = n1.prefix == n2.prefix
         big_numbers = n1.number > 100 and n2.number > 100
-        big_diff = n2.number - n1.number > 100
+        small_diff = n2.number - n1.number < 30
         no_suffix = not n1.suffix and not n2.suffix
         n2_bigger = n2.number > n1.number
         return bool(same_prefix
-                    and (big_numbers or big_diff)
+                    and (big_numbers or small_diff)
+                    and small_diff
                     and no_suffix
                     and n2_bigger)
+
+
+    def short_range(self, n1, n2):
+        """Expands numbers to test for short ranges (e.g., 123456-59)"""
+        x = 10.**(len(str(n2.number)))
+        num = int(math.floor(n1.number / x) * x) + n2.number
+        n2 = SpecNum(n2.code, n2.prefix, num, n2.suffix)
+        return n1, n2
 
 
 
@@ -271,7 +356,7 @@ def get_specimens(catnum, **kwargs):
     response = requests.get(url, headers=headers, params=params)
     print 'Checking {}...'.format(response.url)
     if hasattr(response, 'from_cache') and not response.from_cache:
-        pass # time.sleep(3)
+        time.sleep(1)
     if response.status_code == 200:
         try:
             content = response.json().get('response', {}).get('content', {})
@@ -300,14 +385,16 @@ def filter_records(records, refnum, keywords=None, dept=None):
     if dept is not None:
         dept = depts[dept]
     parser = Parser()
-    refnum = parser.split_num(refnum)
+    try:
+        refnum = parser.parse_num(refnum)
+    except ValueError:
+        return []
     scored = []
     for rec in records:
-        score = 0
+        score = -1
         # Check catalog number
         try:
             catnum = rec['catalogNumber'].upper().split('|')[-1].strip()
-            catnum = parser.split_num(catnum)
             catnum = parser.parse_num(catnum)
         except (IndexError, KeyError, ValueError):
             pass
@@ -329,10 +416,13 @@ def filter_records(records, refnum, keywords=None, dept=None):
                 score -= 100
         # Check taxa against keywords from publication title
         if keywords:
+            # Get taxonomy
             higher_class = rec.get('higherClassification', '').lower()
             taxa = set([t for t in higher_class.split(' | ') if t])
-            matches = taxa & keywords
-            score += len(matches)
+            # Get geological context
+            #context = rec.get('geologicalContextID', '').lower()
+            #names = set([n for n in re.split('[: ]+', context) if len(n) > 5])
+            score += len(taxa & keywords)# + len(names & keywords)
         if score >= 0:
             scored.append([rec, score])
     if scored:
@@ -344,6 +434,17 @@ def filter_records(records, refnum, keywords=None, dept=None):
 if __name__ == '__main__':
     # Test the catalog number parser
     parser = Parser()
-    print 'Testing parser\n-------------'
+    print '-' * 60
+    print 'Testing parser'
+    print '-' * 60
     for val in Parser.regex['test']:
-        print val, '=>', parser.parse(val)
+        matches = parser.findall(val)
+        parsed = []
+        for m in matches:
+            parsed.extend(parser.parse(m))
+        print 'VERBATIM:', val
+        print 'MATCHES: ', matches
+        print 'PARSED:  ', parsed
+        print '-' * 60
+        if Parser.regex['troubleshoot']:
+            break
