75d6212695c1f7730aa88c348bf07c202013cce5
adamancer <mansura@si.edu>
2018-05-30 17:14:02 -0400

    Updated citation app to use Link table and give more context on matches

--- app/api.py
+++ app/api.py
@@ -1,11 +1,13 @@
 import re
 
 from flask import Flask, make_response, render_template, request
+from sqlalchemy import and_
 
 import sys
 sys.path.append('..')
-from database.database import Document, Journal, Specimen, Snippet
+from database.database import Document, Journal, Link, Specimen, Snippet
 from database.queries import Query
+from specimens import get_specimens
 
 
 DB = Query()
@@ -32,31 +34,61 @@ def document(doc_id):
                      Document.topic.label('doc_topic'),
                      Journal.topic.label('jour_topic'),
                      Snippet.snippet,
-                     Specimen.spec_num) \
+                     Link.spec_num,
+                     Link.ezid,
+                     Link.department,
+                     Link.match_quality) \
               .join(Journal, Journal.title == Document.journal) \
               .join(Snippet, Snippet.doc_id == Document.id) \
-              .join(Specimen, Specimen.snippet_id == Snippet.id) \
+              .join(Link, Link.doc_id ==Document.id) \
               .filter(Document.id == doc_id)
     rows = query.all()
     doc = rows[0]
-    spec_nums = []
+    spec_nums = {}
     for row in rows:
-        spec_nums.append(row.spec_num)
-    spec_nums = sorted(list(set(spec_nums)))
+        ezids = [s.strip() for s in row.ezid.split('|')] if row.ezid else []
+        try:
+            spec_nums[row.spec_num]['ezids'].extend(ezids)
+            spec_nums[row.spec_num]['ezids'] = sorted(list(set(spec_nums[row.spec_num]['ezids'])))
+        except:
+            spec_nums[row.spec_num] = {
+                'spec_num': row.spec_num,
+                'ezids': ezids,
+                'dept': row.department,
+                'match_quality': row.match_quality,
+                'snippets': []
+                }
+    spec_nums = spec_nums.values()
+    # Find related snippets
+    query = DB.query(Snippet.snippet, Specimen.spec_num) \
+              .join(Specimen, Specimen.snippet_id == Snippet.id) \
+              .filter(and_(Snippet.doc_id == doc_id))
+    snippets = {}
+    for row in query.all():
+        snippets.setdefault(row.spec_num, []).append(row.snippet)
+    for spec_num in spec_nums:
+        spec_num['snippets'] = snippets[spec_num['spec_num']]
+    print spec_nums
+    spec_nums.sort(key=lambda s: s['spec_num'])
     return render_template('document.htm', doc=doc, spec_nums=spec_nums)
 
 
-@app.route('/specimens/<spec_num>')
-def specimen(spec_num):
-    spec_num = spec_num.replace('+', ' ')
-    query = DB.query(Specimen.spec_num,
-                     Snippet.doc_id,
+@app.route('/specimens/<ezid>')
+def specimen(ezid):
+    # Get basic info about this specimen from the portal
+    metadata = get_specimens(guid=ezid)
+    if metadata:
+        metadata = metadata[0]
+    query = DB.query(Link.spec_num,
+                     Link.ezid,
+                     Link.doc_id,
                      Snippet.snippet,
                      Document.title) \
-              .join(Snippet, Snippet.id == Specimen.snippet_id) \
+              .join(Specimen, Specimen.spec_num == Link.spec_num) \
+              .join(Snippet, and_(Snippet.doc_id == Link.doc_id,
+                                  Specimen.snippet_id == Snippet.id)) \
               .join(Document, Document.id == Snippet.doc_id) \
-              .filter(Specimen.spec_num == spec_num) \
-              .order_by(Document.title)
+              .filter(Link.ezid.like('%' + ezid + '%'))
     rows = query.all()
     specimen = rows[0]
     documents = {}
@@ -66,7 +98,7 @@ def specimen(spec_num):
         doc.setdefault('snippets', {}) \
            .setdefault(format_snippet(row.snippet), []) \
            .append(row.spec_num)
-    return render_template('specimen.htm', specimen=specimen, documents=documents)
+    return render_template('specimen.htm', specimen=specimen, metadata=metadata, documents=documents)
 
 
 def format_snippet(snippet):
