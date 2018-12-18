67c00f2dc13759ddd64d051ffee2c210a1680d08
adamancer <mansura@si.edu>
2018-12-18 07:07:42 -0800

    Updated table definitions

--- database/database.py
+++ database/database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import (Column, Boolean, Integer, String, ForeignKey, create_engine)
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.schema import UniqueConstraint
+from sqlalchemy.schema import Index, UniqueConstraint
 
 
 Base = declarative_base()
@@ -19,6 +19,7 @@ class Document(Base):
     __tablename__ = 'documents'
 
     id = Column(String, primary_key=True)
+    source = Column(String)
     doi = Column(String)
     title = Column(String(collation='nocase'))
     journal = Column(String(collation='nocase'))
@@ -32,12 +33,16 @@ class Snippet(Base):
 
     id = Column(Integer, primary_key=True)
     doc_id = Column(String, ForeignKey('documents.id'), nullable=False)
+    page_id = Column(String)
+    start = Column(Integer)
     snippet = Column(String(collation='nocase'), nullable=False)
     manual = Column(String(collation='nocase'))
     notes = Column(String(collation='nocase'))
     # Ensure that snippets aren't being duplicated
     __table_args__ = (
-        UniqueConstraint('doc_id', 'snippet', name='_doc_snippet'),
+        UniqueConstraint('doc_id', 'page_id', 'snippet', name='_id_snippet'),
+        Index('idx_snippets_doc_id', 'doc_id'),
+        Index('idx_snippets_page_id', 'page_id')
     )
 
 
@@ -48,6 +53,9 @@ class Specimen(Base):
     snippet_id = Column(String, ForeignKey('snippets.id'), nullable=False)
     verbatim = Column(String(collation='nocase'), nullable=False)
     spec_num = Column(String(collation='nocase'), nullable=False)
+    __table_args__ = (
+        Index('idx_specimens_snippet_id', 'snippet_id'),
+    )
 
 
 class Link(Base):
@@ -56,9 +64,11 @@ class Link(Base):
     id = Column(Integer, primary_key=True)
     doc_id = Column(String, ForeignKey('documents.id'), nullable=False)
     spec_num = Column(String(collation='nocase'), nullable=False)
+    corrected = Column(String(collation='nocase'))
     ezid = Column(String)
     match_quality = Column(String(collation='nocase'))
     department = Column(String(collation='nocase'))
+    num_snippets = Column(Integer)
     notes = Column(String(collation='nocase'))
     # Ensure that specimens aren't being duplicated
     __table_args__ = (
@@ -74,5 +84,32 @@ try:
 except OSError:
     pass
 engine = create_engine('/'.join(['sqlite:///', root, 'output', 'specimens.db']))
+class Part(Base):
+    __tablename__ = 'parts'
+
+    id = Column(String, primary_key=True)
+    source = Column(String)
+    item_id = Column(Integer)
+    part_id = Column(Integer)
+    first_page = Column(Integer)
+    min_page = Column(Integer)
+    max_page = Column(Integer)
+    # Define indexes
+    __table_args__ = (
+        Index('idx_parts_item_part_page_ids', 'item_id', 'part_id', 'min_page', 'max_page'),
+    )
+
+
+class Taxon(Base):
+    __tablename__ = 'taxa'
+
+    id = Column(Integer, primary_key=True)
+    source_id = Column(String)
+    taxon = Column(String)
+    # Define indexes
+    __table_args__ = (
+        UniqueConstraint('source_id', 'taxon', name='_src_taxa'),
+        Index('idx_taxa_source_id', 'source_id')
+    )
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
