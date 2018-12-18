06531f165552a341065312b9a7088a105aef73ff
adamancer <mansura@si.edu>
2018-12-18 06:56:30 -0800

    Moved shared functions for clustering, scoring, etc. into miners

---
+++ miners/cluster.py
@@ -0,0 +1,310 @@
+"""Defines functions to cluster and expand catalog numbers found by regex"""
+
+import logging
+import os
+import re
+
+import yaml
+
+
+def epen(fn, *args, **kwargs):
+    fn = os.path.basename(fn)
+    return open(os.path.join(os.path.dirname(__file__), fn), *args, **kwargs)
+
+
+class Cluster(object):
+
+    def __init__(self):
+        self.regex = yaml.load(epen(os.path.abspath('regex.yml'), 'rb'))
+        self.regex['catnum'] = self.regex['catnum'].format(**self.regex)
+        self.mask = re.compile(self.regex['mask'].format(**self.regex))
+        self.simple = re.compile(self.regex['simple'])
+        self.discrete = re.compile(self.regex['discrete_mask'].format(**self.regex))
+        self.suf_range = re.compile(r'(([A-z])' + self.regex['join_range'].format(**self.regex) + r'([A-z]))')
+        self.range = re.compile(self.regex['range_mask'].format(**self.regex))
+        self.code = ''
+        self.codes = [s.strip() for s in self.regex['code'].strip('()').split('|')]
+        self.metadata = []
+        self.expand_short_ranges = True
+        if self.regex['troubleshoot']:
+            logging.basicConfig(level=logging.DEBUG)
+        # A valid last number is any catalog number, suffix, or range. However,
+        # the regexes defined in regex.yml are less selective than is needed
+        # here. We'll limit a "good" value to a subset of high-quality matches.
+        catnum = r'([A-Z]{1,3} ?\d{2,6}|\d{4,6})'
+        self.p_catnum = r'^{catnum}(-{catnum})?$'.format(catnum=catnum)
+        #self.p_catnum = r'^([A-Z]{1,3} ?)?\d{4,6}((-[A-Z]{1,3} ?)?\d{4,6})?$'
+        self.p_suffix = r'^(\d{1,4}|[a-z](-[a-z]|[a-z]+)|[a-z]\d|\d[a-z])$'
+        self.p_alpha_suffix = r'^([a-z](-[a-z]|[a-z]+)?|[a-z]\d|\d[a-z])$'
+
+
+    def split_on_delim(self, val, delim=r'(,|;|\.|&| and )'):
+        """Splits string on common delimiters"""
+        return re.split(delim, val)
+
+
+    def split_into_catnums(self, val):
+        """Splits string based on catalog number regular expression"""
+        pass
+
+
+    def join(self, vals):
+        """Joins values, adjusting spacing for punctuation"""
+        delims = ';,'
+        for delim in delims:
+            if any([re.search('[^A-z0-9]', val) for val in vals]):
+                joined = []
+                for val in vals:
+                    if val in delims:
+                        joined[-1] += val
+                    else:
+                        joined.append(val)
+                return ' '.join(vals).strip()
+        return '; '.join(vals)
+
+
+    def is_valid_catnum(self, val, minlen=4):
+        """Tests if value is a valid catalog number"""
+        result = bool(re.match('^' + self.regex['catnum'] + '$', val))
+        if result:
+            # Check if suffixed numbers are long enough
+            vals = val.split('-')
+            result = len(vals[0]) >= minlen
+        logging.info('"%s" %slooks like a catnum', val, '' if result else 'does not ')
+        return result
+
+
+    def all_valid_catnums(self, vals, minlen=4, discard_delim=True):
+        """Tests if all values in a list are valid catalog numbers"""
+        if not isinstance(vals, list):
+            vals = self.split_on_delim(val)
+        if discard_delim:
+            vals = [s for s in vals if re.search('[A-z0-9]', s)]
+        result = all([self.is_valid_catnum(s.strip(), minlen=minlen) for s in vals])
+        # Test if all values exceed the minimum length
+        if result:
+            result = all([len(s.strip()) > minlen for s in vals])
+        # Test if any value ends with an alpha suffix range
+        if result:
+            result = not any([self.ends_with_range(s.strip()) for s in vals])
+        return result
+
+
+    def ends_with_range(self, val):
+        return bool(re.search(r'[a-z]-[a-z]$', val))
+
+
+
+    def trim_bad_values(self, val):
+        """Trims unlikely catalog numbers/suffixes from a list"""
+        logging.info('Trimming "%s"', val)
+        orig = val
+        # Strip out filler
+        val = re.sub(self.regex['filler'], '', val)
+        # Split on common delimiters
+        vals = self.split_on_delim(val)
+        # Iteratively trim last value
+        while not self._validate_last(vals):
+            vals = vals[:-1]
+        # Go forward through the values, stopping at the first stray alpha
+        for i, val in enumerate(vals):
+            if val.strip().isalpha() and len(val.strip()) > 1:
+                logging.debug('Trimmed "%s" (alphabetic)', val)
+                break
+        vals = vals[:i + 1]
+        if vals != orig:
+            logging.info('Trimmed to %s', vals)
+        return self.join(vals).rstrip(' ,;&')
+
+
+    def _validate_last(self, vals):
+        """Checks if last value in list appears to be a cat number or suffix"""
+        val = vals[-1].strip('# ')
+        if val.replace(' ', '').isdigit() or self.ends_with_range(val):
+            val = val.replace(' ', '')
+        logging.debug('Checking %s...', val)
+        # Always trim non-alphanumeric characters
+        if re.match(r'[^A-z0-9]', val):
+            logging.debug('Trimmed "%s" (not alphanumeric)', val)
+            return False
+        # If multiple values, consider the preceding delimiter. Semicolons
+        # and commas are hard delimiters, and values after these characters
+        # shoulder only be kept if they are either alpha suffixes or obvious
+        # catalog numbers.
+        is_valid = self.is_valid_catnum(val)
+        is_digit = val.isdigit()
+        is_suffix = bool(re.match(self.p_suffix, val))
+        is_alpha_suffix = bool(re.match(self.p_alpha_suffix, val))
+        logging.info('%s: is_valid=%s, is_digit=%s,'
+                     ' is_suffix=%s, is_alpha_suffix=%s',
+                     val, is_valid, is_digit, is_suffix, is_alpha_suffix)
+        if len(vals) > 2:
+            delim = vals[-2].strip()
+            if delim in ',;':
+                # Log each case separately for troubleshooting purposes
+                if is_alpha_suffix and (len(val) > 1 or val not in ('lIO')):
+                    logging.debug('"%s" is a valid alpha suffix', val)
+                elif is_valid and not is_digit:
+                    logging.debug('"%s" is an alphanumeric catnum', val)
+                elif is_valid and is_digit and len(val) >= 4:
+                    logging.debug('"%s" is a numeric catnum 4 digits or longer', val)
+                else:
+                    logging.debug('Trimmed "%s" (weak post-delim value)', val)
+                    return False
+            elif delim == '.' and is_alpha_suffix:
+                return False
+        logging.debug('Stopped trimming at "%s"', val)
+        return True
+
+
+    def expand_alpha_suffixes(self, val):
+        # Get suffixes
+        letters = 'abcdefghijklmnopqrstuvwxyz'
+        # Look for suffix ranges (123456a-c)
+        suf_range = self.suf_range.findall(val)
+        if suf_range:
+            i = letters.index(suf_range[0][1])
+            j = letters.index(suf_range[0][3])
+            suffixes = letters[i:j + 1]
+        else:
+            # Find discrete suffixes (123456a,b,d)
+            suffixes = re.findall(r'(?<![A-z])([A-z])(?![A-z])', val)
+        logging.debug('Suffixes in "%s": %s', val, suffixes)
+        return suffixes
+
+
+    def clean(self, vals):
+        assert isinstance(vals, list)
+        # Clean up the formatting of parts
+        cleaned = []
+        for val in vals:
+            stripped = val.strip()
+            # Remove words
+            val = re.sub('[A-z]{2,}\.?', '', val)
+            # Isolate ranges
+            for suffix in self.suf_range.findall(val):
+                cleaned.append(suffix[0])
+                val = val.replace(suffix[0], '')
+            if len(val) > 1 and not (stripped.isalpha() or stripped.isnumeric() or re.match(self.p_catnum, stripped)):
+                cleaned.extend(list(val))
+            elif val:
+                cleaned.append(val)
+        logging.debug('Cleaned: {}'.format(vals))
+        return vals
+
+
+    def combine(self, vals, minlen=None, maxlen=None, related=None):
+        """Combines fragments and expands suffix ranges
+
+        Numbers are a mix of short and long numbers. This may be a spacing
+        issue, so try combining the numbers semi-intelligently.
+        """
+        if related is None:
+            related = []
+        # Are all values valid catalog numebrs?
+        if self.all_valid_catnums(vals, minlen=4):
+            logging.debug('Aborted: Numbers are already valid')
+            return vals
+        # Are all the numbers minlen digits or longer?
+        nums = [p for p in vals if re.search(r'^(\d+[A-z]?|[A-z](-[A-z])?)$', p)]
+        #if min([len(n) for n in nums]) >= minlen:
+        #    logging.debug('Aborted: Numbers are already the right length')
+        #    return ''.join(vals)
+        related += nums
+        if maxlen is None:
+            maxlen = max([len(n) for n in related])
+        # Can shorter fragments be combined into that length?
+        clustered = []
+        zap_frag = False
+        fragment = ''
+        for orig in vals:
+            val = orig.rstrip(';& ')
+            if re.match('[A-Z]{1,3} ?\d+', val):
+                logging.info('"%s" treated as whole/partial catalog number', val)
+                if fragment and not [n for n in clustered if n.startswith(fragment)]:
+                    clustered.append(fragment)
+                fragment = val
+                zap_frag = False
+            elif val.isnumeric():
+                logging.info('"%s" treated as whole/partial catalog number', val)
+                if len(fragment) == maxlen or zap_frag:
+                    if fragment and not [n for n in clustered if n.startswith(fragment)]:
+                        clustered.append(fragment)
+                    fragment = ''
+                    zap_frag = False
+                fragment += val
+                if len(fragment) > maxlen:
+                    break
+            elif val.isalpha() and len(val) == 1:
+                logging.info('"%s" treated as one-character suffix', val)
+                clustered.append((fragment + val).strip(';'))
+                zap_frag = True
+            elif val != ' ' and fragment:
+                logging.info('"%s" treated as multi-character suffix', val)
+                # Each val may contain one or more suffixes or suffix ranges
+                for val in self.split_on_delim(val):
+                    for suffix in self.expand_alpha_suffixes(val):
+                        clustered.append(fragment + suffix)
+                        zap_frag = True
+            else:
+                logging.warning('"%s" could not be combined', val)
+        else:
+            # Checks if the remaining fragment should be added to clustered
+            if fragment and not [n for n in clustered if n.startswith(fragment)]:
+                if len(fragment) >= minlen:
+                    clustered.append(fragment)
+                else:
+                    logging.debug(u'Aborted: Bad fragment length')
+                    return vals
+            try:
+                nums = [int(n) for n in nums if int(n) in clustered]
+            except ValueError:
+                nums = clustered
+            else:
+                for n in clustered:
+                    mindiff = min([abs(n - m) for m in nums]) if nums else 0
+                    if mindiff > 10:
+                        nums = [n for n in nums if len(str(n)) > minlen]
+                        break
+                else:
+                    nums = clustered
+        logging.info('Combined: %s', nums)
+        return nums
+
+
+    def cluster(self, val, minlen=4, maxlen=6, related=None):
+        """Clusters related digits to better resemble catalog numbers"""
+        if related is None:
+            related = []
+        logging.debug('Clustering...')
+        orig = val
+        logging.debug('Orig: {}'.format(orig))
+        # Format string to improve matches
+        callback = lambda match: match.group(1).lower().strip(' -')
+        val = re.sub('-? ?([A-z], ?[A-z](, ?[A-Z]))', callback, val)
+        # Split off questionable numbers after the last delimiter. Earlier
+        # versions of this part included the hyphen (-), but that doesn't work
+        # well. A hyphen denotes a range, not a list, so it has a different
+        # sense than the other characters included here.
+        val = self.trim_bad_values(val)
+        # Don't try to cluster single numbers
+        if re.search(r'^\d+[a-z]?$', val):
+            logging.debug('Aborted: Value appears to be a single number')
+            return val
+        # Don't try to cluster across different prefixes
+        if re.search(r'\b\d+\b', val) and re.search(r'\b[A-Z]{1,3} ?\d+\b', val):
+            logging.debug('Aborted: Value mixes prefixed and unprefixed numbers')
+            return val
+        # Leave values with range keywords as-is
+        if (re.search(self.regex['join_range'], val)
+            and self.suf_range.search(val) is None):
+            logging.debug('Aborted: Value may be a range')
+            return val
+        parts = [unicode(s) for s in re.split(r'([A-z]*\d+)', val) if s]
+        logging.debug('Parts: {}'.format(parts))
+        if parts:
+            parts = self.combine(self.clean(parts), minlen=minlen,
+                                 maxlen=maxlen, related=related)
+        clustered = self.join(parts)
+        logging.info('Clustered: %s', parts)
+        return clustered
---
+++ miners/link.py
@@ -0,0 +1,366 @@
+import logging
+
+import re
+import time
+
+import requests
+from unidecode import unidecode
+from nltk.corpus import stopwords
+
+from parser import Parser
+
+
+class Score(object):
+    hints = {}
+
+    def __init__(self):
+        self._points = 0
+        self._penalties = 0
+        self.components = {}
+
+
+    def __str__(self):
+        return str(self._points - self._penalties)
+
+
+    def __add__(self, val):
+        if val < 0:
+            self._penalties += val
+        else:
+            self._points += val
+
+
+    def __eq__(self, val):
+        return self.points() == val
+
+
+    def __ne__(self, val):
+        return self.points() != val
+
+
+    def __gt__(self, val):
+        return self.points() > val
+
+
+    def __lt__(self, val):
+        return self.points() < val
+
+
+    def __getitem__(self, key):
+        try:
+            return self.components[key]
+        except KeyError:
+            return 0
+
+
+    def points(self):
+        return self._points + self._penalties
+
+
+    def summary(self, general):
+        # Convert score to match statement
+        matched = []
+        for key in ['higherClassification', 'vernacularName', 'scientificName/catalogNumber']:
+            if self[key] > 0:
+                matched.append('taxa')
+                break
+        for key in ['group/formation/member']:
+            if self[key] > 0:
+                matched.append('stratigraphy')
+                break
+        for key in ['country', 'stateProvince']:
+            if self[key] > 0:
+                matched.append('country/state')
+                break
+        for key in ['municipality/island/verbatimLocality']:
+            if self[key] > 0:
+                matched.append('locality')
+                break
+        for key in ['collectionCode']:
+            if self[key] > 0:
+                matched.append('collection')
+                break
+        for key in ['prefix', 'suffix']:
+            if self[key] > 1:
+                matched.append('catalog')
+                break
+        specifics = ''
+        if matched:
+            specifics = ' (matched {})'.format(', '.join(matched))
+        return '{}{}'.format(general, specifics)
+
+
+    def add(self, keys, val):
+        if not isinstance(keys, list):
+            keys = [keys]
+        key = '/'.join(keys)
+        try:
+            self.components[key] += val
+        except KeyError as e:
+            self.components[key] = val
+        self += val
+        return self
+
+
+    def score(self, catnum, rec, keys, refwords, multiplier=1, match_all=False, **kwargs):
+        if not isinstance(keys, list):
+            keys = [keys]
+        if kwargs:
+            name = kwargs.pop('name', None)
+            if name is not None:
+                name = '|'.join([name] + list(refwords))
+            try:
+                refwords = self.hints[name]
+            except KeyError:
+                refwords = get_keywords(' '.join(refwords), **kwargs)
+                if name is not None:
+                    self.hints[name] = refwords
+        words = [rec[k] for k in keys if rec.get(k) is not None]
+        keywords = get_keywords(' '.join(words), **kwargs)
+        match = None
+        score = 0
+        if keywords:
+            match = keywords & refwords
+            if match:
+                logging.debug('Keyword match: %s (%s)', '; '.join(sorted(list(match))), rec['occurrenceID'])
+            if match_all and len(match) == len(keywords):
+                score = multiplier
+            elif not match_all:
+                score = multiplier * len(match)
+        return self.add(keys, score)
+
+
+def _get_specimens(catnum=None, **kwargs):
+    """Returns specimen metadata from the Smithsonian"""
+    url = 'http://supersite.local/portal'
+    #url = 'https://geogallery.si.edu/portal'
+    headers = {'UserAgent': 'MinSciBot/0.1 (mansura@si.edu)'}
+    params = {
+        'dept': 'any',
+        'format': 'json',
+        'schema': 'simpledwr',
+        'limit': 1000
+    }
+    if catnum is not None:
+        params['sample_id'] = catnum
+    params.update(**kwargs)
+    response = requests.get(url, headers=headers, params=params)
+    #print 'Checking {}...'.format(response.url)
+    if not '.local' in url and hasattr(response, 'from_cache') and not response.from_cache:
+        time.sleep(1)
+    return response
+
+
+def get_specimens(*args, **kwargs):
+    """Returns complete SimpleDarwinCore records from the GeoGallery Portal"""
+    for i in xrange(1, 13):
+        try:
+            response = _get_specimens(*args, **kwargs)
+        except:
+            logging.error('Request failed: %s', args)
+            if i > 3:
+                raise
+        else:
+            if response.status_code == 200:
+                logging.info('Request succeeded: %s (%s)', response.url, response.status_code)
+                try:
+                    records = response.json() \
+                                      .get('response', {}) \
+                                      .get('content', {}) \
+                                      .get('SimpleDarwinRecordSet', [])
+                except AttributeError:
+                    logging.info('No records found')
+                    return []
+                else:
+                    logging.info('%d records found', len(records))
+                    return [rec['SimpleDarwinRecord'] for rec in records]
+            logging.info('Request failed: %s (%s)', response.url, response.status_code)
+        logging.info('Retrying in %d s (retry %d/13)', 2**i, i)
+        time.sleep(2**i)
+
+
+def get_keywords(text, minlen=5, blacklist=None, endings=None, replacements=None):
+    if blacklist is None:
+        blacklist = [
+            'above',
+            'along',
+            'animalia',
+            'beach',
+            'boundary',
+            'coast',
+            'collection',
+            'confluence',
+            'county',
+            'creek',
+            'district',
+            'early',
+            'eastern',
+            'family',
+            'formation',
+            'harbor',
+            'indet',
+            'island',
+            'late',
+            'locality',
+            'lower',
+            'member',
+            'middle',
+            'mountain',
+            'national',
+            'north',
+            'northern',
+            'northeast',
+            'northeastern',
+            'northwest',
+            'northwestern',
+            'genus',
+            'group',
+            'present',
+            'province',
+            'ridge',
+            'river',
+            'slide',
+            'slope',
+            'south',
+            'southern',
+            'southeast',
+            'southeastern',
+            'southwest',
+            'southwestern',
+            'sp',
+            'specimen',
+            'states',
+            'united',
+            'unknown',
+            'upper',
+            'valley',
+            'western',
+            # COLORS
+            'blue',
+            'green',
+            'red',
+            'yellow',
+            'white',
+            'black'
+            ]
+        blacklist.extend(stopwords.words('english'))
+    keywords = []
+    words = unidecode(u'{}'.format(text).lower()).split()
+    for word in words:
+        word = word.strip('.:;,-!?()')
+        if (re.search(r'^[A-Za-z]+$', word)
+            and len(word) >= minlen
+            and word not in blacklist):
+            # Strip endings
+            if endings is not None:
+                for ending in endings:
+                    if word[-len(ending):] == ending:
+                        word = word[:-len(ending)]
+            # Replacements
+            if replacements is not None:
+                for find, repl in replacements.iteritems():
+                    word = word.replace(find, repl)
+            if len(word) > 2:
+                keywords.append(word)
+    return set([kw for kw in keywords if kw])
+
+
+def filter_records(records, refnum, keywords=None, dept=None):
+    """Returns records that match a reference catalog number"""
+    logging.debug('Filtering matches for %s', refnum)
+    depts = {
+        'an': 'Anthropology',
+        'bt': 'Botany',
+        'br': 'Vertebrate Zoology: Birds',
+        'en': 'Entomology',
+        'fs': 'Vertebrate Zoology: Fishes',
+        'hr': 'Vertebrate Zoology: Herpetology',
+        'iz': 'Invertebrate Zoology',
+        'mm': 'Vertebrate Zoology: Mammals',
+        'ms': 'Mineral Sciences',
+        'pl': 'Paleobiology'
+    }
+    if dept is not None:
+        dept = depts.get(dept, dept)
+        if dept not in depts.values():
+            raise ValueError('Bad department: {}'.format(dept))
+    parser = Parser()
+    try:
+        refnum = parser.parse_num(refnum)
+    except ValueError:
+        return []
+    scored = []
+    #if len(records) == 1000:
+    #    raise ValueError('Too many matches: {}'.format(refnum))
+    for rec in records:
+        score = Score()
+        # Check catalog number
+        try:
+            catnum = rec['catalogNumber'].upper().split('|')[-1].strip()
+            catnum = parser.parse_num(catnum)
+        except (IndexError, KeyError, ValueError) as e:
+            # Hack to catch ento type numbers
+            recnums = [r.strip() for r
+                       in rec.get('recordNumber', '').upper().split('|')
+                       if r.strip() == str(refnum.number)]
+            try:
+                catnum = parser.parse_num(recnums[0])
+            except IndexError:
+                catnum = None
+        if catnum is not None:
+            # Exclude records with one-character prefixes if the refnum
+            # is not prefixed. Other departments appear to have prefixes for
+            # internal use (e.g., paleo uses PAL and V) that are not (or are
+            # not always) given when that specimen is cited in the literature.
+            if not refnum.prefix and catnum.prefix and len(catnum.prefix) == 1:
+                score.add('prefix', -1)
+            if catnum.prefix != refnum.prefix and catnum.prefix == 'SD':
+                score.add('prefix', -1)
+            # Exclude records that don't have the same base number
+            if catnum.number != refnum.number:
+                score.add('number', -100)
+            # Bonus point for matching prefix or suffix
+            if score >= 0 and refnum.prefix and catnum.prefix == refnum.prefix:
+                score.add('prefix', 1)
+            if score >= 0 and refnum.suffix and catnum.suffix == refnum.suffix:
+                score.add('suffix', 1)
+        # Check collectionCode against topic
+        if dept is not None:
+            if rec.get('collectionCode') == dept:
+                score.add('collectionCode', 1)
+            else:
+                score.add('collectionCode', -100)
+        # Check taxa against keywords
+        if keywords:
+            # Get taxonomy
+            endings = [
+                'idae',
+                'ian',
+                'ide',
+                'ine',
+                'ia',
+                'us',
+                's',
+                'a',
+                'e'
+            ]
+            replacements = {
+                'aeo': 'eo',  # archaeo
+                'usc': 'usk'  # mollusk
+            }
+            if rec.get('collectionCode') != 'Mineral Sciences':
+                score.score(catnum, rec, 'higherClassification', keywords, multiplier=5, name='hc', endings=endings, replacements=replacements)
+                score.score(catnum, rec, 'vernacularName', keywords, multiplier=3, match_all=True)
+            else:
+                score.score(catnum, rec, ['scientificName', 'catalogNumber'], keywords, multiplier=3, match_all=True, name='vn', endings=['ic', 'y'])
+            if rec.get('collectionCode') in ['Mineral Sciences', 'Paleobiology']:
+                score.score(catnum, rec, ['group', 'formation', 'member'], keywords, multiplier=3)
+            score.score(catnum, rec, 'country', keywords, multiplier=0.51, match_all=True)
+            score.score(catnum, rec, 'stateProvince', keywords, multiplier=0.51, match_all=True)
+            score.score(catnum, rec, ['municipality', 'island', 'verbatimLocality'], keywords)
+        if score.points() > 1:
+            scored.append([rec, score])
+    if scored:
+        max_score = max([s[1] for s in scored])
+        return [m for m in scored if m[1] == max_score]
+    return []
