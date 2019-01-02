a0b7cf101ebe64c0ab8772187e38b5ce6f95f1c2
adamancer <mansura@si.edu>
2019-01-02 07:24:51 -0800

    Drop-in replacement with auto-retry with backoff for simple requests

---
+++ miners/rerequests.py
@@ -0,0 +1,54 @@
+from __future__ import print_function
+
+import logging
+import time
+
+import requests
+
+
+class ReRequest(object):
+
+    def __init__(self, max_retries=8, base_wait=2, min_wait=2, stop_codes=None):
+        self.stop_codes = stop_codes if stop_codes is not None else [404]
+        self.max_retries = max_retries
+        self.base_wait = base_wait
+        self.min_wait = min_wait
+        self._methods = {
+            'get': requests.get,
+            'post': requests.post
+        }
+
+
+    def retry(self, method, *args, **kwargs):
+        wait = 0
+        i = 0
+        while i <= self.max_retries:
+            if wait:
+                url = response.request.url
+                msg = '{}: {}. Waiting {} seconds to retry (retries={})...'.format(url, code, wait, 1)
+                print(msg)
+                logging.debug(msg)
+                time.sleep(wait)
+            response = self._methods[method](*args, **kwargs)
+            code = response.status_code
+            if code == 200 or code in self.stop_codes:
+                return response
+            # Calculate next wait
+            i, wait = self.backoff(i, code)
+            i += 1
+
+
+    def _backoff(self, i, code):
+        return i, self.min_wait + self.base_wait ** i
+
+
+    def backoff(self, i, code):
+        return self._backoff(i, code)
+
+
+    def get(self, *args, **kwargs):
+        return self.retry('get', *args, **kwargs)
+
+
+    def post(self, *args, **kwargs):
+        return self.retry('post', *args, **kwargs)
